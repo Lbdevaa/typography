df129c7a1e34ec87d2c6319e55b1ccb71debe499
Merak <mesheryakov97@gmail.com>
2019-11-04 23:17:38 +0500

    finish adaptive

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,17 @@
 $(function() {
+	// бургер
+	document.querySelector('.burger-menu').onclick = function() {
+		this.classList.toggle('active'); 
+		document.querySelector('.mobile-menu').classList.toggle("show") 
+	}  
 
-	// Custom JS
-
+	// весь каталог
+	document.querySelector('.pop-pagination').onclick = function() {
+		this.classList.toggle('active');  
+		document.querySelector('.mobile-more').classList.toggle("show") 
+	} 
+	var mobMore = document.querySelectorAll('.mobile-more__list'); 
+	mobMore.forEach( mobMore => mobMore.onclick = function(){
+			this.classList.toggle('show');  
+	} )
 });
